3d66fd68e73677e8dc9580ba8fce9d9be02b47ce
Niks Home computer <nsumikawa@gmail.com>
2019-02-14 07:44:54 -0700

    updated wsgi to point to nxp_tools

--- django_config/wsgi.py
+++ django_config/wsgi.py
@@ -21,15 +21,15 @@ from common.utils.operating_system.logger import initialize_logging
 
 #add path for server
 if platform.node() == 'az84cqc01':
-    sys.path.append('/home/nxa18331/git/excursion')
-    sys.path.append('/home/nxa18331/git/excursion/django_config')
+    sys.path.append('/home/nxa18331/git/nxp_tools')
+    sys.path.append('/home/nxa18331/git/nxp_tools/django_config')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_config.settings")
 
 django.setup()
 
 #initialize the logger
-if platform.node() == 'az84cqc01':path = '/home/nxa18331/logs/web/excursion_detection'
+if platform.node() == 'az84cqc01':path = '/home/nxa18331/logs/web/nxp_tools'
 else : path = '%s/logs' % os.path.dirname(os.path.abspath(__file__))
 create_folder( path )
 initialize_logging( path=path, level=logging.DEBUG )
