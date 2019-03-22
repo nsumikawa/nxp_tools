0c4d5ca6ee460e332a19a033874453789939f6ae
Niks Home computer <nsumikawa@gmail.com>
2019-03-22 08:22:09 -0700

    added broken page

--- doc/urls.py
+++ doc/urls.py
@@ -6,23 +6,8 @@ from ajax.views import category, element, document, search
 urlpatterns = [
     # ex: /
     url(r'^$', views.test, name='test'),
-    # url(r'^$', views.training_doc, name='training_doc'),
-    # url(r'^training_doc/$', views.training_doc, name='training_doc'),
-    # url(r'^training_doc/(?P<dir_id>[0-9]+)/$', views.training_doc, name='training_doc'),
-    #
-    #
-    # url(r'^add_doc/$', views.update_document, name='add_doc'),
-    # url(r'^add_doc/(?P<event_id>[0-9]+)/$', views.update_document, name='add_doc'),
-    #
-    #
-    # url(r'^search/$', views.search, name='search'),
-    # url(r'^new_documents/$', views.new_documents, name='new_documents'),
-    #
-    #
-    # url(r'^training_faq/$', views.training_faq, name='training_faq'),
-    # url(r'^training_faq/(?P<dir_id>[0-9]+)/$', views.training_faq, name='training_faq'),
-    # url(r'^add_faq/$', views.update_FAQ, name='add_faq'),
-    # url(r'^add_faq/(?P<event_id>[0-9]+)/$', views.update_FAQ, name='add_faq'),
+    url(r'^broken/$', views.broken, name='broken'),
+
 ]
 
 urlpatterns += category.urlpatterns
